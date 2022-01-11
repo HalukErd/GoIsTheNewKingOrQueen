fa523197c166cb3f028c21c25543f9962caf476f
halukerd <erd.haluk@gmail.com>
2022-01-12 01:45:20 +0300

    Maps

--- main.go
+++ main.go
@@ -3,5 +3,22 @@ package main
 import "log"
 
 func main() {
-	log.Fatal("Check Branches.")
+	//var  colors map[string]string
+
+	//colors := make(map[int]string)
+	//colors[10] = "#ff00ff"
+
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4fb450",
+		"white": "#ffffff",
+	}
+
+	printColors(colors)
+}
+
+func printColors(c map[string]string) {
+	for color, hex := range c {
+		log.Println("Hex code for", color, "is", hex)
+	}
 }
