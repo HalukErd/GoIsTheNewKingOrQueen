2f5047fe786b17c7d9dfc2b428f56185618a4193
halukerd <erd.haluk@gmail.com>
2022-01-11 23:19:53 +0300

    Structs And Pointers

--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"log"
+)
+
+type contactInfo struct {
+	email       string
+	phoneNumber int
+}
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
 
 func main() {
-	log.Fatal("Check Branches.")
+
+	me := person{
+		firstName: "haluk",
+		lastName:  "erd",
+		contactInfo: contactInfo{
+			email:       "erd.haluk@gmail.com",
+			phoneNumber: 5454555454,
+		},
+	}
+	//mePointer := &me
+	//mePointer.updateName("Luciano")
+	me.updateName("Luciano")
+	me.print()
+	fmt.Printf("%+v", *&me)
+}
+
+func (pointerToPerson *person) updateName(newName string) {
+	(*pointerToPerson).firstName = newName
+}
+
+func (pointerToPerson person) print() {
+	log.Printf("%+v", pointerToPerson)
 }
