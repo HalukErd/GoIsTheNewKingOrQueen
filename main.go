d2f8a0b4a940c845cecaf3cf99c15a0012cddb34
halukerd <erd.haluk@gmail.com>
2022-01-14 00:40:23 +0300

    Interface

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "log"
+type bot interface {
+	getGreeting() string
+}
+type englishBot struct{}
+type spanishBot struct{}
+type turkishBot struct{}
 
 func main() {
-	log.Fatal("Check Branches.")
+	var eb englishBot
+	var sb spanishBot
+	var tb turkishBot
+	printGreeting(eb)
+	printGreeting(sb)
+	printGreeting(tb)
+}
+
+func printGreeting(b bot) {
+	println(b.getGreeting())
+}
+
+//func printGreeting(eb englishBot) {
+//	eb.getGreeting()
+//}
+//func printGreeting(sb spanishBot) {
+//	sb.getGreeting()
+//}
+//func printGreeting(tb turkishBot) {
+//	tb.getGreeting()
+//}
+
+func (eb englishBot) getGreeting() string {
+	return "Hello!"
+}
+
+func (spanishBot) getGreeting() string {
+	return "Hola!"
+}
+
+func (turkishBot) getGreeting() string {
+	return "Selam!"
 }
