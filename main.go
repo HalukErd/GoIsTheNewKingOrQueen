48f84379eeb1fe063cd2ad8234ece1a7c51c6e16
halukerd <erd.haluk@gmail.com>
2022-01-15 01:27:29 +0300

    Channels And Go Routines

--- main.go
+++ main.go
@@ -1,7 +1,51 @@
 package main
 
-import "log"
+import (
+	"log"
+	"net/http"
+	"time"
+)
 
 func main() {
-	log.Fatal("Check Branches.")
+	links := []string{
+		"http://google.com",
+		"http://amazon.com",
+		"http://facebook.com",
+		"http://golang.org",
+		"http://stackoverflow.com",
+	}
+	c := make(chan string)
+
+	//for _, link := range links {
+	//	checkLink(link, c)
+	//}
+
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+
+	//for i := 0; i < len(links); i++ {
+	//	log.Println(<-c)
+	//}
+	for l := range c {
+		//time.Sleep(10 * time.Second)
+		//go checkLink(l, c)
+		go func(link string) {
+			time.Sleep(5 * time.Second)
+			checkLink(link, c)
+		}(l)
+	}
+
+	//time.Sleep(5 * time.Second)
+}
+
+func checkLink(l string, c chan string) {
+	_, err := http.Get(l)
+	if err != nil {
+		log.Println(l, "is down.")
+		c <- l
+		return
+	}
+	log.Println(l, "is up.")
+	c <- l
 }
