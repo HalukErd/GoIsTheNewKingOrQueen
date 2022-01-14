cfb06cbdf2de0eb29e10d152755cdc0d5079767a
halukerd <erd.haluk@gmail.com>
2022-01-15 01:38:25 +0300

    Interface Assignment

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "log"
+import "fmt"
+
+type shape interface {
+	getArea() float64
+}
+
+type triangle struct {
+	base   float64
+	height float64
+}
+type square struct {
+	length float64
+}
 
 func main() {
-	log.Fatal("Check Branches.")
+	t := triangle{base: 10, height: 20}
+	s := square{length: 15}
+	printArea(t)
+	printArea(s)
+}
+
+func (t triangle) getArea() float64 {
+	return 0.5 * t.height * t.base
+}
+
+func (s square) getArea() float64 {
+	return s.length * s.length
+}
+
+func printArea(s shape) {
+	fmt.Printf("Area: %v\n", s.getArea())
 }
