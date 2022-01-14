0b04786986cf0a6466dba25dec29ece43e4402bc
halukerd <erd.haluk@gmail.com>
2022-01-15 02:26:50 +0300

    Interface Assignment Reader Writer

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "log"
+import (
+	"io"
+	"os"
+	"regexp"
+)
 
 func main() {
-	log.Fatal("Check Branches.")
+	for _, fn := range os.Args {
+		matched, errMatch := regexp.MatchString("/*.txt", fn)
+		if errMatch == nil && matched {
+			f, err := os.Open(fn)
+			if err == nil {
+				io.Copy(os.Stdout, f)
+			}
+		}
+	}
 }
