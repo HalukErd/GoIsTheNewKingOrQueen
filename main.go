a8afa3908ad215f18c38277317698c14c08abad8
halukerd <erd.haluk@gmail.com>
2022-01-26 10:34:01 +0300

    initial server

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
 
 func main() {
-	log.Fatal("Check Branches.")
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Panic(err)
+	}
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Panic(err)
+		}
+
+		io.WriteString(conn, "\nHello Tcp Server\n")
+		fmt.Fprintln(conn, "You're my daily now")
+		fmt.Fprintln(conn, "So today was yesterday")
+
+		conn.Close()
+	}
+
 }
