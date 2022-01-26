04eb7178e86f014c36cfe35e4e75a05a095945c6
halukerd <erd.haluk@gmail.com>
2022-01-27 02:38:50 +0300

    url creator initial.

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "log"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
 
 func main() {
-	log.Fatal("Check Branches.")
+	s := os.Args[1]
+	file, err := os.Open("p.txt")
+
+	url1 := `<a href="https://bitbucket.org/avansashybristeam/avansas/branches/?search=%v">  ______  Search:%v  ______  </a>`
+	url2 := `<a href="https://bitbucket.org/avansashybristeam/avansas/branch/master?dest=%v#diff">  ______  Merge Master %v  ______  </a>`
+	url3 := `<a href="https://bitbucket.org/avansashybristeam/avansas/branch/%v?dest=%v">  ______  PR %v to %v  ______  </a>`
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	scnnr := bufio.NewScanner(file)
+
+	for scnnr.Scan() {
+		branch := scnnr.Text()
+		fmt.Printf(url1, branch, branch)
+		fmt.Println("")
+		fmt.Printf(url2, branch, branch)
+		fmt.Println("")
+		fmt.Printf(url3, branch, s, branch, s)
+		fmt.Println("")
+	}
+	io.Copy(os.Stdout, file)
 }
