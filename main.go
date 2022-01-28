7d717da64a530c0b0da5d1ae707fc9ed79e865a5
halukerd <erd.haluk@gmail.com>
2022-01-29 00:15:16 +0300

    ServeHTTP(ResponseWriter, *http.Request)

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"net/http"
+)
+
+type hndlr string
+
+func (h hndlr) ServeHTTP(resWriter http.ResponseWriter, req *http.Request) {
+	fmt.Fprintln(resWriter, "Here is the place you should code")
+}
 
 func main() {
-	log.Fatal("Check Branches.")
+	var h hndlr
+	http.ListenAndServe(":8080", h)
 }
