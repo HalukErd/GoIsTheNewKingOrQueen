0eed469c54bf0263fd13e47c40565ced411b54cb
halukerd <erd.haluk@gmail.com>
2022-01-29 00:53:51 +0300

    Http and template

--- main.go
+++ main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
+	"text/template"
 )
+
+var tpl *template.Template
 
 type hndlr string
 
-func (h hndlr) ServeHTTP(resWriter http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(resWriter, "Here is the place you should code")
+func (h hndlr) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	err := req.ParseForm()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	tpl.ExecuteTemplate(w, "index.gohtml", req.Form)
+
+}
+
+func init() {
+	tpl = template.Must(template.ParseFiles("index.gohtml"))
 }
 
 func main() {
