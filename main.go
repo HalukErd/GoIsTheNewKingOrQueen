ee4c73a0ce1e334fe4b48a55d45b4a73ebe1841a
halukerd <erd.haluk@gmail.com>
2022-01-29 04:10:55 +0300

    Using Third Party package

--- main.go
+++ main.go
@@ -2,26 +2,34 @@ package main
 
 import (
 	"fmt"
+	"github.com/julienschmidt/httprouter"
 	"io"
+	"log"
 	"net/http"
 )
 
-func login(w http.ResponseWriter, req *http.Request) {
+func login(w http.ResponseWriter, req *http.Request, params httprouter.Params) {
 	fmt.Fprintln(w, "Hey hey")
 	io.WriteString(w, "Hey hey2\n")
 	fmt.Fprintln(w, "Please Login")
 }
 
-func signup(w http.ResponseWriter, req *http.Request) {
+func signup(w http.ResponseWriter, req *http.Request, params httprouter.Params) {
 	fmt.Fprintln(w, "Hey hey")
 	io.WriteString(w, "Hey hey2\n")
 	fmt.Fprintln(w, "Please Register")
 }
 
+func loginProcess(w http.ResponseWriter, req *http.Request, params httprouter.Params) {
+	uid := params.ByName("username")
+	fmt.Fprintln(w, "Welcome", uid)
+}
+
 func main() {
+	rot := httprouter.New()
+	rot.GET("/login", login)
+	rot.GET("/register", signup)
+	rot.GET("/login/:username", loginProcess)
 
-	http.Handle("/login", http.HandlerFunc(login))
-	http.Handle("/register", http.HandlerFunc(signup))
-
-	http.ListenAndServe(":8080", nil)
+	log.Fatal(http.ListenAndServe(":8080", rot))
 }
