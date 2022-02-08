d62a495980d4460a3d9435e57be34156be6bab8e
halukerd <erd.haluk@gmail.com>
2022-02-09 01:46:27 +0300

    Done

--- main.go
+++ main.go
@@ -9,50 +9,49 @@ import (
 func mathString(str string) string {
 	str = editBefore(str)
 	str = noBracket(str)
-	str = calc(str)
 	return str
 }
 
 func editBefore(str string) string {
 	var strB strings.Builder
-
+	strB.WriteString("(")
 	for i, ch := range str {
 		if ch == ')' && i != len(str)-1 && !strings.ContainsAny(str[i+1:i+2], "+-*/") {
 			strB.WriteString(")*")
-			//} else if ch == '(' && i != 0 && !strings.ContainsAny(str[i-1:i], "+-*/") {
-			//	strB.WriteString("*(")
 		} else if ch == '-' {
 			strB.WriteString("+-")
 		} else {
 			strB.WriteRune(ch)
 		}
 	}
+	strB.WriteString(")")
 	return strB.String()
 }
 
 func noBracket(str string) string {
 	var strB strings.Builder
 
-	for i, ch := range str {
+	for i := 0; i < len(str); i++ {
 
-		if ch == '(' {
-			strB.WriteString(noBracket(str[i+1:]))
-			return strB.String()
-		} else if ch == ')' {
+		if str[i] == '(' {
+			str = str[:i] + noBracket(str[i+1:])
+			i--
+		} else if str[i] == ')' {
 			strB.Reset()
 			strB.WriteString(calc(str[:i]))
-			if i != len(str)-1 {
-				strB.WriteString(noBracket(str[i+1 : len(str)]))
-			}
+			strB.WriteString(str[i+1:])
 			return strB.String()
 		} else {
-			strB.WriteRune(ch)
+			strB.WriteRune(rune(str[i]))
 		}
 	}
 	return strB.String()
 }
 
 func calc(str string) string {
+	if str[0] == '+' || str[0] == '-' {
+		return str
+	}
 	sl := strings.Split(str, "+")
 	var sum int
 	for i := 0; i < len(sl); i++ {
@@ -99,5 +98,5 @@ func miniOp(a int, op rune, b int) int {
 }
 
 func main() {
-	fmt.Println(mathString("1+22*(3+1)(35-33*2+33)-2*(3)"))
+	fmt.Println(mathString("1+22*(3+1)(35-(33*2)+33)-2*(3)"))
 }
